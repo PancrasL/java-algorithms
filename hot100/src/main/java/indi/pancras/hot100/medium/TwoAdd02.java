bdef3525d797112b5846e8b05564c691b7c0032f
PancrasL <pengzelv@qq.com>
2021-12-30 15:15:51 +0800

    two sum two add

---
+++ hot100/src/main/java/indi/pancras/hot100/medium/TwoAdd02.java
@@ -0,0 +1,34 @@
+package indi.pancras.hot100.medium;
+
+import indi.pancras.hot100.ListNode;
+
+public class TwoAdd02 {
+    class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            int carry = 0;
+            int val = 0;
+            // 头节点
+            ListNode head = new ListNode(0);
+            ListNode cur = head;
+            while (l1 != null || l2 != null) {
+                int l1val = l1 == null ? 0 : l1.val;
+                int l2val = l2 == null ? 0 : l2.val;
+                int sum = l1val + l2val + carry;
+                carry = sum / 10;
+                val = sum % 10;
+                cur.next = new ListNode(val);
+                cur = cur.next;
+                if (l1 != null) {
+                    l1 = l1.next;
+                }
+                if (l2 != null) {
+                    l2 = l2.next;
+                }
+            }
+            if (carry == 1) {
+                cur.next = new ListNode(carry);
+            }
+            return head.next;
+        }
+    }
+}
