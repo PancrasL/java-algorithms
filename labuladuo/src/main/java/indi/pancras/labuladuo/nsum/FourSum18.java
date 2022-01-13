28d339d13998b37a2966dcd2b2fb1784d326867d
PancrasL <pengzelv@qq.com>
2022-01-13 13:24:15 +0800

    两数之和系列

---
+++ labuladuo/src/main/java/indi/pancras/labuladuo/nsum/FourSum18.java
@@ -0,0 +1,49 @@
+package indi.pancras.labuladuo.nsum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourSum18 {
+    class Solution {
+        public List<List<Integer>> fourSum(int[] nums, int target) {
+            List<List<Integer>> result = new ArrayList<>();
+            if (nums.length <= 3) {
+                return result;
+            }
+            Arrays.sort(nums);
+            for (int aIndex = 0; aIndex < nums.length; aIndex++) {
+                for (int bIndex = aIndex + 1; bIndex < nums.length; bIndex++) {
+                    int abSum = nums[aIndex] + nums[bIndex];
+                    int cIndex = bIndex + 1;
+                    int dIndex = nums.length - 1;
+                    while (cIndex < dIndex) {
+                        int cdSum = nums[cIndex] + nums[dIndex];
+                        if (abSum + cdSum == target) {
+                            result.add(Arrays.asList(nums[aIndex], nums[bIndex], nums[cIndex], nums[dIndex]));
+                            while (cIndex < dIndex && nums[cIndex] == nums[cIndex + 1]) {
+                                cIndex++;
+                            }
+                            cIndex++;
+                            while (cIndex < dIndex && nums[dIndex - 1] == nums[dIndex]) {
+                                dIndex--;
+                            }
+                            dIndex--;
+                        } else if (abSum + cdSum < target) {
+                            cIndex++;
+                        } else {
+                            dIndex--;
+                        }
+                    }
+                    while (bIndex + 1 < nums.length && nums[bIndex] == nums[bIndex + 1]) {
+                        bIndex++;
+                    }
+                    while (aIndex < bIndex && nums[aIndex] == nums[aIndex + 1]) {
+                        aIndex++;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ labuladuo/src/main/java/indi/pancras/labuladuo/nsum/ThreeSum15.java
@@ -0,0 +1,47 @@
+package indi.pancras.labuladuo.nsum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum15 {
+    class Solution {
+        public List<List<Integer>> threeSum(int[] nums) {
+            List<List<Integer>> result = new ArrayList<>();
+            if (nums.length <= 2) {
+                return result;
+            }
+            Arrays.sort(nums);
+
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] > 0) {
+                    return result;
+                }
+                // 跳过重复元素
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                int left = i + 1, right = nums.length - 1;
+                while (left < right) {
+                    int sum = nums[left] + nums[right];
+                    if (nums[i] + sum == 0) {
+                        result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right - 1] == nums[right]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    } else if (nums[i] + sum < 0) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
