51c0f1325d669001d8fe2e33b744e5104ce2d728
PancrasL <pengzelv@qq.com>
2022-01-20 17:02:39 +0800

    dynamic

--- labuladuo/src/main/java/indi/pancras/labuladuo/dynamic/CoinChange322.java
+++ labuladuo/src/main/java/indi/pancras/labuladuo/dynamic/CoinChange322.java
@@ -7,7 +7,7 @@ public class CoinChange322 {
         public int coinChange(int[] coins, int amount) {
             // counts[i]表示兑换i元的最少硬币数
             int[] counts = new int[amount + 1];
-            Arrays.fill(counts,-1);
+            Arrays.fill(counts, -1);
             counts[0] = 0;
             for (int i = 0; i <= amount; i++) {
                 if (counts[i] == -1) {
@@ -27,6 +27,25 @@ public class CoinChange322 {
                 }
             }
             return counts[amount];
+        }
+    }
+
+    class Solution1 {
+        public int coinChange(int[] coins, int amount) {
+            int[] dp = new int[amount + 1];
+            Arrays.fill(dp, Integer.MAX_VALUE);
+            dp[0] = 0;
+            for (int i = 1; i <= amount; i++) {
+                for (int coin : coins) {
+                    if (i - coin < 0) {// 子问题无解
+                        continue;
+                    }
+                    if (dp[i - coin] != Integer.MAX_VALUE) {
+                        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                    }
+                }
+            }
+            return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
         }
     }
 }
