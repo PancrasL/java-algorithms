5cb091ab23e49a2d77465652027d87b817fe296a
lvpengze <lvpengze.lpz@alibaba-inc.com>
2022-06-24 18:01:46 +0800

    two

--- hot100/src/main/java/indi/pancras/hot100/firsttime/TwoAdd02.java
+++ hot100/src/main/java/indi/pancras/hot100/firsttime/TwoAdd02.java
@@ -3,32 +3,34 @@ package indi.pancras.hot100.firsttime;
 import indi.pancras.hot100.ListNode;
 
 public class TwoAdd02 {
-    class Solution {
-        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-            int carry = 0;
-            int val = 0;
-            // 头节点
-            ListNode head = new ListNode(0);
-            ListNode cur = head;
-            while (l1 != null || l2 != null) {
-                int l1val = l1 == null ? 0 : l1.val;
-                int l2val = l2 == null ? 0 : l2.val;
-                int sum = l1val + l2val + carry;
-                carry = sum / 10;
-                val = sum % 10;
-                cur.next = new ListNode(val);
-                cur = cur.next;
-                if (l1 != null) {
-                    l1 = l1.next;
-                }
-                if (l2 != null) {
-                    l2 = l2.next;
-                }
-            }
-            if (carry == 1) {
-                cur.next = new ListNode(carry);
-            }
-            return head.next;
+
+  class Solution {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+      ListNode head = new ListNode();
+      ListNode cur = head;
+
+      int sum = 0, carry = 0;
+      while (l1 != null || l2 != null) {
+        sum = 0;
+        if (l1 != null) {
+          sum += l1.val;
+          l1 = l1.next;
         }
+        if (l2 != null) {
+          sum += l2.val;
+          l2 = l2.next;
+        }
+        sum += carry;
+        carry = sum / 10;
+        sum %= 10;
+        cur.next = new ListNode(sum);
+        cur = cur.next;
+      }
+      if (carry == 1) {
+        cur.next = new ListNode(1);
+      }
+      return head.next;
     }
+  }
 }
--- hot100/src/main/java/indi/pancras/hot100/firsttime/TwoSum01.java
+++ hot100/src/main/java/indi/pancras/hot100/firsttime/TwoSum01.java
@@ -3,17 +3,22 @@ package indi.pancras.hot100.firsttime;
 import java.util.HashMap;
 
 public class TwoSum01 {
-    class Solution {
-        public int[] twoSum(int[] nums, int target) {
-            HashMap<Integer, Integer> map = new HashMap<>();
-            for (int i = 0; i < nums.length; i++) {
-                int num = nums[i];
-                if (map.containsKey(target - num)) {
-                    return new int[]{map.get(target - num), i};
-                }
-                map.put(num, i);
-            }
-            return null;
+
+  class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+      if (nums == null || nums.length < 2) {
+        return new int[0];
+      }
+      HashMap<Integer, Integer> map = new HashMap<>();
+      for (int i = 0; i < nums.length; i++) {
+        if (map.containsKey(target - nums[i])) {
+          return new int[]{map.get(target - nums[i]), i};
+        } else {
+          map.put(nums[i], i);
         }
+      }
+      return new int[0];
     }
+  }
 }
